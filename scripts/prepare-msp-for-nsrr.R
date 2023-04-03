b447e3b55f265693dcfe6f15eef812ef53756ea7
M-Tully <mtully853@gmail.com>
2023-04-03 11:38:20 -0400

    Add nsrr variable source, visit, fileid variables

--- scripts/prepare-msp-for-nsrr.R
+++ scripts/prepare-msp-for-nsrr.R
@@ -11,12 +11,25 @@ demo <- read_sav("//rfawin/BWH-SLEEPEPI-NSRR-STAGING/20221018-dipietro-fhr/NSSRD
 colnames(demo) <- tolower(colnames(demo))
 demo$visit = 1
 
+#add leading 0's where needed
+demo$id[nchar(demo$id)==1] <- paste("00",demo$id[nchar(demo$id)==1],sep="")
+demo$id[nchar(demo$id)==2] <- paste("0",demo$id[nchar(demo$id)==2],sep="")
+
+# create fileid variable to match EDF
+demo$fileid <- paste("msp-S",demo$id,sep="")
+
+#reorder
+demo<-demo[,c("id","fileid",colnames(demo)[2:25])]
+
+#write first dataset
 setwd(paste("C:/Users/mkt27/msp-data-dictionary/csvs/",ver, sep=""))
 write.csv(demo, paste("msp-dataset-",ver,".csv",sep=""), row.names = F)
 
+#now harmonized
 demo2 <- data.frame(id = demo$id,
+                    fileid = demo$fileid,
                     nsrr_age = demo$mat_age,
-                    nsrr_age_gt89 = "yes",
+                    nsrr_age_gt89 = "no",
                     nsrr_race = NA,
                     nsrr_sex = "female",
                     nsrr_current_smoker = "no")
