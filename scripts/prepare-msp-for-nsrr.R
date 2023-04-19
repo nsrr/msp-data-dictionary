a5a3ae01672f4ff3f0ef23e55bd376f2e83bc958
M-Tully <mtully853@gmail.com>
2023-04-19 15:04:29 -0400

    PSG harmonization, PSG variable metadata expanded

--- scripts/prepare-msp-for-nsrr.R
+++ scripts/prepare-msp-for-nsrr.R
@@ -1,47 +1,58 @@
 #-------------------------------------------------------------------#
-#      Convert SPSS file to csv and prep for NSRR MSPF dataset      #
+#      Convert SPSS file to csv and prep for NSRR MSP dataset       #
 #      Date Created: 29-01-23      Author: Meg Tully                # 
 #-------------------------------------------------------------------#
 
 ver <- "0.1.0.pre"
 
 library(haven)
-demo <- read_sav("//rfawin/BWH-SLEEPEPI-NSRR-STAGING/20221018-dipietro-fhr/NSSRDEMOVARIABLES.SAV")
+data <- read_sav("//rfawin/BWH-SLEEPEPI-NSRR-STAGING/20221018-dipietro-fhr/NSSRdemoVARIABLES.SAV")
 
-colnames(demo) <- tolower(colnames(demo))
-demo$visit = 1
+colnames(data) <- tolower(colnames(data))
+data$visit = 1
 
 #add leading 0's where needed
-demo$id[nchar(demo$id)==1] <- paste("00",demo$id[nchar(demo$id)==1],sep="")
-demo$id[nchar(demo$id)==2] <- paste("0",demo$id[nchar(demo$id)==2],sep="")
+data$id[nchar(data$id)==1] <- paste("00",data$id[nchar(data$id)==1],sep="")
+data$id[nchar(data$id)==2] <- paste("0",data$id[nchar(data$id)==2],sep="")
 
 # create fileid variable to match EDF
-demo$fileid <- paste("msp-S",demo$id,sep="")
+data$fileid <- paste("msp-S",data$id,sep="")
 
 #reorder
-demo<-demo[,c("id","fileid",colnames(demo)[2:25])]
+data<-data[,c("id","fileid",colnames(data)[2:25])]
 
 #write first dataset
 setwd(paste("C:/Users/mkt27/msp-data-dictionary/csvs/",ver, sep=""))
-write.csv(demo, paste("msp-dataset-",ver,".csv",sep=""), row.names = F)
+write.csv(data, paste("msp-dataset-",ver,".csv",sep=""), row.names = F)
 
-#now harmonized
-demo2 <- data.frame(id = demo$id,
-                    fileid = demo$fileid,
-                    nsrr_age = demo$mat_age,
+#now create harmonized dataset
+
+#datagraphics:
+data2 <- data.frame(id = data$id,
+                    fileid = data$fileid,
+                    nsrr_age = data$mat_age,
                     nsrr_age_gt89 = "no",
                     nsrr_race = NA,
                     nsrr_sex = "female",
                     nsrr_current_smoker = "no")
-for(i in 1:length(demo$id)){
-  if(demo$mat_race[i]==2){
-    demo2$nsrr_race[i] <- "black or african american"}
-  if(demo$mat_race[i]==1){
-    demo2$nsrr_race[i] <- "white"}
-  if(demo$mat_race[i]==3){
-    demo2$nsrr_race[i] <- "asian"}
-}
 
-demo2$visit = 1
+for(i in 1:length(data$id)){
+  if(data$mat_race[i]==2){
+    data2$nsrr_race[i] <- "black or african american"}
+  if(data$mat_race[i]==1){
+    data2$nsrr_race[i] <- "white"}
+  if(data$mat_race[i]==3){
+    data2$nsrr_race[i] <- "asian"}}
 
-write.csv(demo2, paste("msp-dataset-harmonized-",ver,".csv",sep=""), row.names = F)
+# PSG harmonized variables
+
+data2$nsrr_ahi_hp3u <- data$ahi1
+data2$nsrr_ahi_hp3r_aasm15 <- data$ahi2
+data2$nsrr_ttleffbd	<- data$se
+data2$nsrr_ttldursp_f1 <- data$tst
+data2$nsrr_pctdursp_s3_f1	<- data$tstdeep
+data2$nsrr_pctdursp_sr_f1	<- data$tstrem
+
+data2$visit = 1
+
+write.csv(data2, paste("msp-dataset-harmonized-",ver,".csv",sep=""), row.names = F)
